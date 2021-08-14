bef82aa8cb0560c90c8db685d8685791bbc8b19a
Kushal Sen <kuse@microsoft.com>
2021-08-14 12:15:01 +0530

    AddnNumbers

--- AddNumbers.c
+++ AddNumbers.c
@@ -8,13 +8,15 @@ int add2Numbers(int a, int b) // function definition : Question what is the diff
 // Error :  Compilation/Runtime/Logical
 int main()
 {
-	int a,sum=0,i;  //,c,sum;  
+	int a,sum=0,i,n;  //,c,sum;  
 	
-	for( i = 1 ; i <= 12 ; i++)  //  i++   ==  i=i+1
+	printf("how many number do you want to add ?");
+	scanf("%d",&n);
+	for( i = 1 ; i <= n ; i++)  //  i++   ==  i=i+1
 	{
 		printf("enter %d number",i);
 		scanf("%d",&a);
-		sum = add2Numbers(sum,a);
+		sum = add2Numbers(sum,a);  // sum = sum + a    or   sum+=a;
 	}
 	
 
@@ -29,4 +31,4 @@ int main()
 	printf("sum:%d",sum);
 }
 
-//Create a calculator to add 12 positive numbers and we can guarantee that the numbers and the result can fit in "INT" 
+//Create a calculator to add 'n' positive numbers and we can guarantee that the numbers and the result can fit in "INT" 
