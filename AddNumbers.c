03e2caf21594bc7ad69e91a17aa808105ad54168
Kushal Sen <kuse@microsoft.com>
2021-08-14 11:44:29 +0530

    AddByTakingInput

--- AddNumbers.c
+++ AddNumbers.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
-int add2Numbers(int,int);
-int add2Numbers(int a, int b)
+int add2Numbers(int,int); // function declaration 
+int add2Numbers(int a, int b) // function definition : Question what is the difference and What is the need?
 {
 	 return a+b;
 }
+// Error :  Compilation/Runtime/Logical
 int main()
 {
-	int a = 6;
-	int sum = add2Numbers(a,4);
+	int a,b,c,sum;
+	printf("enter first number ");
+	scanf("%d",&a);
+	printf("enter second number ");
+	scanf("%d",&b);
+	
+	printf("enter third number ");
+	scanf("%d",&c);
+	
+	sum = add2Numbers(a,b,c);
 	printf("sum:%d",sum);
 }
