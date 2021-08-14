c4554097a9c9a94ca94743c3c0df765e1303ea30
Kushal Sen <kuse@microsoft.com>
2021-08-14 12:26:57 +0530

    IsEvenOrOdd

--- AddNumbers.c
+++ AddNumbers.c
@@ -5,19 +5,42 @@ int add2Numbers(int a, int b) // function definition : Question what is the diff
 {
 	 return a+b;
 }
+bool isEvenNumber(int); // function declaration for checking is even or not   : output   true if number is even, else false
+bool isEvenNumber(int a)
+{
+	if(0 == a%2) // if a is divided by 2   check if the remainder is 0
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 // Error :  Compilation/Runtime/Logical
 int main()
 {
 	int a,sum=0,i,n;  //,c,sum;  
 	
-	printf("how many number do you want to add ?");
+	printf("check if a number is even or not, enter the number ?");
 	scanf("%d",&n);
-	for( i = 1 ; i <= n ; i++)  //  i++   ==  i=i+1
+	
+	if(isEvenNumber(n))
 	{
-		printf("enter %d number",i);
-		scanf("%d",&a);
-		sum = add2Numbers(sum,a);  // sum = sum + a    or   sum+=a;
+		printf("It is even");
 	}
+	else
+	{
+		printf("It is odd");
+	}
+	
+	// for( i = 1 ; i <= n ; i++)  //  i++   ==  i=i+1
+	// {
+		// printf("enter %d number",i);
+		// scanf("%d",&a);
+		// sum = add2Numbers(sum,a);  // sum = sum + a    or   sum+=a;
+	// }
+	
 	
 
 	//printf("enter second number ");
