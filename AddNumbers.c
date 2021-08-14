393750602990029d538f16a91f25b8a1303000cb
Kushal Sen <kuse@microsoft.com>
2021-08-14 11:29:17 +0530

    v2

--- AddNumbers.c
+++ AddNumbers.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+int add2Numbers(int,int);
+int add2Numbers(int a, int b)
+{
+	 return a+b;
+}
 int main()
 {
 	int a = 6;
-	int sum = a+ 3;
+	int sum = add2Numbers(a,4);
 	printf("sum:%d",sum);
 }
